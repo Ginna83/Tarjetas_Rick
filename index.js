d483cd86ea853811c974931fa0787c118468f8f0
Ginna Marcela Mondragon Gallego <1111666828@u.icesi.edu.co>
2023-03-24 17:57:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const charater = document.querySelector("post")
+
+function createPost (name , message , imageURL){
+    return
+    <div class= "tarjetas">
+    <h1><imag class= "imagen" drc="${imageURL}" alt= "" ></h1>
+    <h2>${name}</h2>
+    <p>$</p>
+    
+    
+
+    </div>
+}
